29459a9e149616bfbc93af6803a354759a473119
Kam Woods <kamwoods@gmail.com>
2015-01-16 23:13:01 -0500

    Temp update of bootstrap

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -4,7 +4,9 @@
 # ----------------------------------------
 # Include parameteres file
 # ------------------------
-source /vagrant/.vagrant_bootstrap/parameters.sh
+# source /vagrant/.vagrant_bootstrap/parameters.sh
+# source ./parameters.sh
 # Update the box release repositories
 # -----------------------------------
 apt-get update
+apt-get install ubuntu-desktop
