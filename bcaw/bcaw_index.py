10ffc06f7d06b4b7ecb805f517704ac7e9d03254
Sunitha Misra <sunithanc@yahoo.com>
2015-05-06 09:29:01 -0400

    First version of the file containing Lucene indexing utility.

---
+++ bcaw/bcaw_index.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+# coding=UTF-8
+#
+# BitCurator Access Webtools (Disk Image Access for the Web)
+# Copyright (C) 2014
+# All rights reserved.
+#
+# This code is distributed under the terms of the GNU General Public
+# License, Version 3. See the text file "COPYING" for further details
+# about the terms of this license.
+#
+# This file is part of the BitCurator Access Webtools application.
+# Code is used from the samples provided in Lucene download package.
+# It provides routines to index the given text files and to search for
+# a specified string using these indexes.
+#
+INDEX_DIR = "IndexFiles.index"
+
+import sys, os, lucene, threading, time
+from datetime import datetime
+
+from java.io import File
+from org.apache.lucene.analysis.miscellaneous import LimitTokenCountAnalyzer
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.document import Document, Field, FieldType
+from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig
+from org.apache.lucene.store import SimpleFSDirectory
+from org.apache.lucene.util import Version
+from org.apache.lucene.queryparser.classic import QueryParser
+
+class IndexFiles(object):
+    """ IndexFiles takes the root of a directory structure and the destination
+        directory as arguments to create Lucene indexes for the directory 
+        and copy the indexs into the target directory. 
+        Attributes:
+        root: Root of the directory structure containing files to be indexed.
+        store_dir: Output directory where indexes are to be stored. 
+    """
+    def __init__(self, root, store_dir):
+
+        if not os.path.exists(store_dir):
+            os.mkdir(store_dir)
+
+        # NOTE: Hardcoded the analyzer instead of passing it
+        analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
+        store = SimpleFSDirectory(File(store_dir))
+        analyzer = LimitTokenCountAnalyzer(analyzer, 1048576)
+        config = IndexWriterConfig(Version.LUCENE_CURRENT, analyzer)
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+        writer = IndexWriter(store, config)
+
+        self.indexDocs(root, writer)
+        ticker = Ticker()
+        print 'commit index',
+        threading.Thread(target=ticker.run).start()
+        writer.commit()
+        writer.close()
+        ticker.tick = False
+        print 'done'
+
+    def indexDocs(self, root, writer):
+
+        t1 = FieldType()
+        t1.setIndexed(True)
+        t1.setStored(True)
+        t1.setTokenized(False)
+        t1.setIndexOptions(FieldInfo.IndexOptions.DOCS_AND_FREQS)
+
+        t2 = FieldType()
+        t2.setIndexed(True)
+        t2.setStored(False)
+        t2.setTokenized(True)
+        t2.setIndexOptions(FieldInfo.IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
+
+        for root, dirnames, filenames in os.walk(root):
+            for filename in filenames:
+                if not filename.endswith('.txt'):
+                    continue
+                print "adding", filename
+                try:
+                    path = os.path.join(root, filename)
+                    file = open(path)
+                    contents = unicode(file.read(), 'iso-8859-1')
+                    file.close()
+                    doc = Document()
+                    doc.add(Field("name", filename, t1))
+                    doc.add(Field("path", root, t1))
+                    if len(contents) > 0:
+                        doc.add(Field("contents", contents, t2))
+                    else:
+                        print "warning: no content in %s" % filename
+                    writer.addDocument(doc)
+                except Exception, e:
+                    print "Failed in indexDocs:", e
+
+def searchIndexedFiles(searcher, analyzer, search_text):
+    """ Searces the Lucene index created by indexDocs for the given string,
+        search_text
+    """
+    search_list = []
+    print "Searching for:", search_text
+    query = QueryParser(Version.LUCENE_CURRENT, "contents",
+                        analyzer).parse(search_text)
+    scoreDocs = searcher.search(query, 50).scoreDocs
+    print "%s total matching documents." % len(scoreDocs)
+
+    if len(scoreDocs) == 0:
+        print "Query: Not found for : ",search_text
+        return None
+    else:
+        for scoreDoc in scoreDocs:
+            doc = searcher.doc(scoreDoc.doc)
+            ## print 'D: searchIndexedFiles: path:', doc.get("path"), 'name:', doc.get("name")
+            file_path = doc.get("path") + "/" + doc.get("name")
+            search_list.append(file_path)
+        ## print "D: searchIndexFiles: Search List from Lucene Indexing ", search_list
+        return search_list
