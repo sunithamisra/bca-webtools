665cb088ae1fc1978351755bdf6696243740c65d
Sunitha Misra <sunithanc@yahoo.com>
2015-07-23 00:36:07 -0400

    Added config options for filename indexing

--- bcaw_default_settings.py
+++ bcaw_default_settings.py
@@ -5,3 +5,8 @@ IMAGEDIR = '/vagrant/disk-images'
 DELETED_FILES = True
 INDEX_DIR = "/vagrant/lucene_index"
 FILES_TO_INDEX_DIR = "/vagrant/files_to_index"
+# FILESEARCH_DB: If set, searches for the filenames in the DB as opposed to
+# searching the index
+FILESEARCH_DB = True
+# FILE_IDEXDIR is the directory where the index for filename search is stored.
+FILENAME_INDEXDIR = "/vagrant/filenames_to_index"
